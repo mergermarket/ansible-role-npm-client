7cdcae510e67f922513c7f103aa809766ae95736
Marcio Goda <marciogoda@gmail.com>
2020-02-19 16:34:58 +0000

    Fixed unit test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -3,12 +3,10 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
+def test_build_npm_config_cron_exists(host):
+    f = host.file('/etc/cron.d/build-npm-config')
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
