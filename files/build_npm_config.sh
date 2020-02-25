14840592d7178fc6955a4c2e2c9d554af3b15187
Marcio Goda <marciogoda@gmail.com>
2020-02-25 11:19:43 +0000

    Fixed docker run script

--- files/build_npm_config.sh
+++ files/build_npm_config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker pull mergermarket/npm_repository_config:latest
 
-docker run -it --rm \
+docker run --rm \
         -e AWS_ACCESS_KEY_ID \
         -e AWS_SECRET_ACCESS_KEY \
         -e AWS_SESSION_TOKEN \
         -e AWS_DEFAULT_REGION \
         -v ~/:/home \
-        mergermarket/npm_repository_config python build_npmrc.py "platform/$JENKINS_ENV/jenkins_npm_repository_config"
+        mergermarket/npm_repository_config python build_npmrc.py platform/$JENKINS_ENV/jenkins_npm_repository_config
