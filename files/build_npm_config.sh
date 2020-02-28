75e9e6ed8f06b25dafeaba88daa0c174a908db51
Arthur Gassner <arthur.gassner@gmail.com>
2020-02-28 11:48:42 +0000

    Tweaks to get it working

--- files/build_npm_config.sh
+++ files/build_npm_config.sh
@@ -4,9 +4,10 @@ docker pull mergermarket/npm_repository_config:latest
 export $(cat /var/lib/jenkins/swarm_env | xargs)
 
 docker run --rm \
-        -e AWS_ACCESS_KEY_ID \
-        -e AWS_SECRET_ACCESS_KEY \
-        -e AWS_SESSION_TOKEN \
-        -e AWS_DEFAULT_REGION \
-        -v ~/:/home \
-        mergermarket/npm_repository_config python build_npmrc.py platform/$JENKINS_ENV/jenkins_npm_repository_config
+    -e AWS_ACCESS_KEY_ID \
+    -e AWS_SECRET_ACCESS_KEY \
+    -e AWS_SESSION_TOKEN \
+    -e AWS_DEFAULT_REGION \
+    -v /etc:/home \
+    -u $(id -u):$(id -g) \
+    mergermarket/npm_repository_config python build_npmrc.py platform/$JENKINS_ENV/jenkins_npm_repository_config
