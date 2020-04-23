6d9af3b8fe215b052c71333b6830b1beb22dc821
keir2 <keir.badger@acuris.com>
2020-04-23 14:21:33 +0100

    fix region issue

--- files/build_npm_config.sh
+++ files/build_npm_config.sh
@@ -7,7 +7,7 @@ docker run --rm \
     -e AWS_ACCESS_KEY_ID \
     -e AWS_SECRET_ACCESS_KEY \
     -e AWS_SESSION_TOKEN \
-    -e AWS_DEFAULT_REGION \
+    -e AWS_DEFAULT_REGION=eu-west-1 \
     -v ~/:/home \
     -u $(id -u):$(id -g) \
     mergermarket/npm_repository_config python build_npmrc.py /home/env.json
